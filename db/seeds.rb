6b10efa02326c6a2d15881b15c3138fbca913bcf
Hashim Gari <hashmaster3k@gmail.com>
2021-02-13 09:40:22 -0700

    update seed to read csv and import to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+# Added to measure seeding times
+starting = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+
+street_cafes = []
+CSV.foreach('./Street Cafes 2020-21.csv', headers: true, header_converters: :symbol) do |data|
+  street_cafes << StreetCafe.new(name: data[:cafrestaurant_name], address: data[:street_address], post_code: data[:post_code], num_chairs: data[:number_of_chairs])
+end
+StreetCafe.import(street_cafes)
+ActiveRecord::Base.connection.reset_pk_sequence!('street_cafes')
+
+ending = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+elapsed = ending - starting
+puts "\nSuccess! Took #{elapsed.round(2)} seconds to seed the database.\n\n"
