251e4ca0c73eb61b4294aa8459b0dfcf04b2f0ff
Hashim Gari <hashmaster3k@gmail.com>
2021-02-14 22:43:53 -0700

    refactor street cafe model and testing

--- app/models/street_cafe.rb
+++ app/models/street_cafe.rb
@@ -2,27 +2,11 @@ class StreetCafe < ApplicationRecord
   validates :name, :address, :post_code, :num_chairs, presence: true
 
   def self.find_medium_large_cafes
-    cafes = where('category != ?', 'other').or(where.not(category: nil))
-    medium_large_cafes = []
-
-    cafes.each do |cafe|
-      category = cafe.category.split(' ').second
-      medium_large_cafes << cafe if category == 'medium' || category == 'large'
-    end
-
-    medium_large_cafes
+    where('category LIKE ? OR category LIKE ?', "%medium%", "%large%")
   end
 
   def self.find_small_cafes
-    cafes = where('category != ?', 'other').or(where.not(category: nil))
-    small_cafes = []
-
-    cafes.each do |cafe|
-      category = cafe.category.split(' ').second
-      small_cafes << cafe if category == 'small'
-    end
-
-    small_cafes
+    where('category LIKE ?', "%small%")
   end
 
   def self.delete_cafes(list_cafes)
--- spec/models/street_cafe_spec.rb
+++ spec/models/street_cafe_spec.rb
@@ -60,4 +60,69 @@ RSpec.describe StreetCafe, type: :model do
       expect(StreetCafe.count).to eq(0)
     end
   end
+
+  describe 'class methods' do
+    before :each do
+      StreetCafe.create!(
+        name: 'Cafe 1',
+        address: '1',
+        post_code: 'LS1 123',
+        num_chairs: 5, category: 'ls1 small')
+      StreetCafe.create!(
+        name: 'Cafe 2',
+        address: '2',
+        post_code: 'LS1 234',
+        num_chairs: 10, category: 'ls1 medium')
+      StreetCafe.create!(
+        name: 'Cafe 3',
+        address: '3',
+        post_code: 'LS1 345',
+        num_chairs: 100,
+        category: 'ls1 large')
+      StreetCafe.create!(
+        name: 'Cafe 4',
+        address: '4',
+        post_code: 'LS2 456',
+        num_chairs: 6,
+        category: 'ls2 small')
+      StreetCafe.create!(
+        name: 'Cafe 5',
+        address: '5',
+        post_code: 'LS2 567',
+        num_chairs: 9,
+        category: 'ls2 small')
+      StreetCafe.create!(
+        name: 'Cafe 6',
+        address: '6',
+        post_code: 'LS2 678',
+        num_chairs: 30,
+        category: 'ls2 large')
+      StreetCafe.create!(
+        name: 'Cafe 7',
+        address: '7',
+        post_code: 'LS2 890',
+        num_chairs: 145,
+        category: 'ls2 large')
+      StreetCafe.create!(
+        name: 'Cafe 8',
+        address: '8',
+        post_code: 'LS9 123',
+        num_chairs: 42,
+        category: 'other')
+    end
+
+    it 'gets all medium and large cafes' do
+      expect(StreetCafe.find_medium_large_cafes.count).to eq(4)
+    end
+
+    it 'gets all small cafes' do
+      expect(StreetCafe.find_small_cafes.count).to eq(3)
+    end
+
+    it 'deletes an array of given cafes' do
+      expect(StreetCafe.count).to eq(8)
+      StreetCafe.delete_cafes(StreetCafe.all)
+      expect(StreetCafe.count).to eq(0)
+    end
+  end
 end
